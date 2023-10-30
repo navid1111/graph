9f2c546d1f002a217a8dea2b932e44912184d679
navid-kamal <navidkamal@iut-dhaka.edu>
2023-10-30 12:22:38 +0600

    isBipartite

---
+++ bipertite.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define fr first
+#define sc second
+
+bool dfs(int node, int col, int color[], vector<int>adj[]) {
+	color[node] = col;
+	for (auto it : adj[node]) {
+		if (color[it] == col)return false;
+		else if (dfs(it, col, color, adj) == false)return false;
+
+		return true;
+	}
+
+}
+bool isBipertite(int V, vector<int>adj[]) {
+	int vis[V] = { -1};
+	for (int i = 0; i < V; i++) {
+		if (vis[i] == -1) {
+			if (dfs(i, 0, vis, adj) == false)
+				return false;
+		}
+	}
+	return true;
+
+}
+void addEdge(vector <int> adj[], int u, int v) {
+	adj[u].push_back(v);
+	adj[v].push_back(u);
+}
+
+int main() {
+
+	// V = 4, E = 4
+	vector<int>adj[4];
+
+	addEdge(adj, 0, 2);
+	addEdge(adj, 0, 3);
+	addEdge(adj, 2, 3);
+	addEdge(adj, 3, 1);
+
+
+	bool ans = isBipertite(4, adj);
+	if (ans)cout << "1\n";
+	else cout << "0\n";
+
+	return 0;
+}
